6d9cc1cff5ff7f2bfbc113ffb4e32ad4dd2909e7
Max <maksym.illarionov@nure.ua>
2022-02-21 16:49:27 +0200

    Implemented methods Calc, Multiply and Result which are called by delegates.

---
+++ Module3HW3/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Module3HW3
+{
+    public class Class1
+    {
+        public Action<bool> ShowDelegate { get; set; }
+
+        public int Multiply(int firstValue, int secondValue) => firstValue * secondValue;
+    }
+}
---
+++ Module3HW3/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Module3HW3
+{
+    public class Class2
+    {
+        private int MultiplyResult { get; set; }
+
+        public bool Result(int number)
+        {
+            return MultiplyResult % number == 0;
+        }
+
+        public Predicate<int> Calc(Func<int, int, int> multiplyDelegate, int firstValue, int secondValue)
+        {
+            MultiplyResult = multiplyDelegate.Invoke(firstValue, secondValue);
+            return new Predicate<int>(Result);
+        }
+    }
+}
